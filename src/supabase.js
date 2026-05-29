31c7f5e6eb236ca1bbb0a6a7185fed5cfe5f4003
gabriel-effiong <gabrielekpenyong55@gmail.com>
2026-05-29 03:15:17 +0100

    added supabase auth

---
+++ src/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl =
+  "https://supabase.com/dashboard/project/zbmuvcegrtdhlwthhtaa/settings/api-keys";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpibXV2Y2VncnRkaGx3dGhodGFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwMDM3MDEsImV4cCI6MjA5NTU3OTcwMX0.US3YMlrdgPh2e0Z-vY2jq8pSoOZiiIvYTW9XgVhh2R4";
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
